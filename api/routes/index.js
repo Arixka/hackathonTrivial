2364d5f50f9e2c6fbe6e9f2134400cb00ed06223
Maria Siverio <marisiver25@gmail.com>
2021-06-10 17:35:52 +0100

    trivia incomplete 1.0.2

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { userRouter } = require('./user.router')
 const { questionRouter } = require('./question.router')
-const { createRoom } = require('./room.router')
+// const { createRoom } = require('./room.router')
 
 router
   .use('/users', userRouter)
   .use('/questions', questionRouter)
-  .use('/rooms', createRoom)
+  // .use('/rooms', createRoom)
 
 exports.router = router
